5ae91f071792c5d6d4595e412a5b7737f007694b
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 17:14:39 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,8 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-	socket.on("close", () => {
+	socket.on("data", (data) => {
+		console.log(data.toString());
 		socket.write("HTTP/1.1 200 OK\r\n\r\n");
 		socket.end();
 	});
