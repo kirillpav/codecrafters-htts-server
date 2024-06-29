1c760d13cf0af03e4c8d75e74ca8843867737a5f
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 17:59:42 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -23,7 +23,7 @@ const server = net.createServer((socket: any) => {
 			socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 		}
 	});
-	socket.end();
+	// socket.end();
 });
 
 server.listen(4221, "localhost");
