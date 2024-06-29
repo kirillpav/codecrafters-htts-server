8226d8f14ab3b82fe07d883a1aaefc7f19327b87
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 17:28:47 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -8,11 +8,14 @@ const server = net.createServer((socket) => {
 	socket.on("data", (data) => {
 		const request = data.toString();
 		const path = request.split(" ")[1];
+		let response = "";
 		if (path === "/") {
-			socket.write("HTTP/1.1 200 OK\r\n\r\n");
+			response = "HTTP/1.1 200 OK\r\n\r\n";
 		} else {
-			socket.write('"HTTP/1.1 404 Not Found\r\n\r\n"');
+			response = "HTTP/1.1 404 Not Found\r\n\r\n";
 		}
+
+		socket.write(response);
 	});
 });
 
