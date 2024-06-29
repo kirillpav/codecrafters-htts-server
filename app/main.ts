2e902c197f294c75d26a50bad9df5d2f98430f44
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 17:26:38 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -6,9 +6,13 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
 	socket.on("data", (data) => {
-		console.log(data.toString());
-		socket.write("HTTP/1.1 200 OK\r\n\r\n");
-		socket.end();
+		const request = data.toString();
+		const path = request.split(" ")[1];
+		if (path === "/") {
+			socket.write("HTTP/1.1 200 OK\r\n\r\n");
+		} else {
+			socket.write('"HTTP/1.1 404 Not Found\r\n\r\n"');
+		}
 	});
 });
 
