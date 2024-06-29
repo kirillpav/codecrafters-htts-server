3840ea6f23b956f29d4b05ca69b589c82a0c9c65
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 17:51:10 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -4,19 +4,25 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-const server = net.createServer((socket) => {
-	socket.on("data", (data) => {
+const server = net.createServer((socket: any) => {
+	socket.on("data", (data: any) => {
 		const request = data.toString();
-		const path = request.split(" ")[1];
-		let response = "";
-		if (path === "/") {
-			response = "HTTP/1.1 200 OK\r\n\r\n";
-		} else {
-			response = "HTTP/1.1 404 Not Found\r\n\r\n";
-		}
 
-		socket.write(response);
+		const path = request.split(" ")[1];
+
+		const query = path.split("/")[2];
+
+		if (path === "/") {
+			socket.write("HTTP/1.1 200 OK\r\n");
+		} else if (path === `/echo/${query}`) {
+			socket.write(
+				`HTTP/1.1 200 OK\r\n Content-Type: text/plain\r\n Content-Length: ${query.length}\r\n\r\n ${query}`
+			);
+		} else {
+			socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+		}
 	});
+	socket.end();
 });
 
 server.listen(4221, "localhost");
