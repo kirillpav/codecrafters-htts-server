c89bb645079f1f2e6dbd1df18e637aee59304ac7
kirill pavlov <2020kirillp@gmail.com>
2024-06-29 18:06:39 +0300

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,29 +1,29 @@
 import * as net from "net";
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket: any) => {
+	console.log("Client is connected");
 	socket.on("data", (data: any) => {
-		const request = data.toString();
-
-		const path = request.split("\r\n")[0].split(" ")[1];
-		console.log(path);
-
+		const dataStr = data.toString();
+		console.log("Data received: ", dataStr);
+		const path = dataStr.split("\r\n")[0].split(" ")[1];
+		console.log("path", path);
 		const query = path.split("/")[2];
-
+		console.log("pathBody", query);
 		if (path === "/") {
-			socket.write("HTTP/1.1 200 OK\r\n");
+			socket.write("HTTP/1.1 200 OK\r\n\r\n");
 		} else if (path === `/echo/${query}`) {
 			socket.write(
-				`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n ${query}`
+				`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${query.length}\r\n\r\n${query}`
 			);
 		} else {
 			socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
 		}
+		console.log("Client diconnecting");
+		socket.end();
 	});
-	// socket.end();
 });
-
-server.listen(4221, "localhost");
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+console.log("Logs from your program will appear here!");
+// Uncomment this to pass the first stage
+server.listen(4221, "localhost", () => {
+	console.log("Server is running on port 4221");
+});
